6c0bd2ab428e4375e6c0e0da4ebc94157892397e
poflannery-od <padraig.flannery@onedigital.com>
2025-08-11 18:39:08 -0600

    removed remote header change

--- app/(homepage)/layout.tsx
+++ app/(homepage)/layout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { cookies } from "next/headers";
 import Footer from "@/components/structure/Footer";
 import Header from "@/components/structure/Header";
 
@@ -8,9 +7,6 @@ const HomepageLayout = async ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  const cookieStore = await cookies();
-  const cookieValue = cookieStore.get("cookies-consent-fcllc")?.value;
-
   return (
     <div>
       <Header />
--- components/structure/Header.tsx
+++ components/structure/Header.tsx
@@ -14,55 +14,27 @@ const Header = () => {
   const scrollPosition = useScroll();
   const mobile = useIsMobile();
 
-  if (mobile) {
-    return (
-      <div className="fixed top-0 w-screen bg-white border-b-[1px] border-b-black/30 z-50 h-[80px] flex items-center justify-center px-4">
-        <div className="flex items-center justify-between max-w-7xl w-full mx-auto">
-          <Link href="/">
-            <Image
-              src={appConfig.logo.src}
-              alt={appConfig.logo.alt}
-              width={50}
-              height={50}
-            />
-          </Link>
-          <Button
-            variant="outline"
-            className="text-sm font-light text-black/"
-            asChild
-          >
-            <Link
-              className="flex items-center justify-center space-x-2"
-              href="/#contact-us"
-            >
-              Contact Us <ArrowRightIcon className="w-5 h-5" strokeWidth={1} />
+  return (
+    <>
+      {scrollPosition > 50 && (
+        <motion.div
+          key="header"
+          className="fixed top-0 w-screen bg-white border-b-[1px] border-b-black/30 z-50 h-[80px] flex items-center justify-center px-8"
+          initial={{ y: -80 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.5 }}
+          exit={{ y: -80 }}
+        >
+          <div className="flex items-center justify-between max-w-7xl w-full mx-auto">
+            <Link href="/">
+              <Image
+                src={appConfig.logo.src}
+                alt={appConfig.logo.alt}
+                width={50}
+                height={50}
+              />
             </Link>
-          </Button>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <>
-        {scrollPosition > 50 && (
-          <motion.div
-            key="header"
-            className="fixed top-0 w-screen bg-white border-b-[1px] border-b-black/30 z-50 h-[80px] flex items-center justify-center px-8"
-            initial={{ y: -80 }}
-            animate={{ y: 0 }}
-            transition={{ duration: 0.5 }}
-            exit={{ y: -80 }}
-          >
-            <div className="flex items-center justify-between max-w-7xl w-full mx-auto">
-              <Link href="/">
-                <Image
-                  src={appConfig.logo.src}
-                  alt={appConfig.logo.alt}
-                  width={50}
-                  height={50}
-                />
-              </Link>
-              {/* <div className="flex items-center justify-center space-x-4">
+            {/* <div className="flex items-center justify-center space-x-4">
                 <Link
                   className="text-sm font-light text-black/80 hover:text-black/30 transition-all ease-linear duration-150"
                   href="#"
@@ -82,24 +54,23 @@ const Header = () => {
                   Services
                 </Link>
               </div> */}
-              <Button
-                variant="outline"
-                className="text-sm font-light text-black/"
-                asChild
+            <Button
+              variant="outline"
+              className="text-sm font-light text-black/"
+              asChild
+            >
+              <Link
+                className="flex items-center justify-center space-x-2"
+                href="/#contact-us"
               >
-                <Link
-                  className="flex items-center justify-center space-x-2"
-                  href="/#contact-us"
-                >
-                  Contact Us{" "}
-                  <ArrowRightIcon className="w-5 h-5" strokeWidth={1} />
-                </Link>
-              </Button>
-            </div>
-          </motion.div>
-        )}
-      </>
-    );
-  }
+                Contact Us{" "}
+                <ArrowRightIcon className="w-5 h-5" strokeWidth={1} />
+              </Link>
+            </Button>
+          </div>
+        </motion.div>
+      )}
+    </>
+  );
 };
 export default Header;
